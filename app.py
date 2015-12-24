e5e23815f3af1cf1e223331c175e79220781a11f
Ishan Khare <ishankhare07@gmail.com>
2015-12-24 16:39:23 +0530

    moved to RESTful api

--- app.py
+++ app.py
@@ -1,66 +1,93 @@
 from user import app, db, User
 from flask_sqlalchemy import orm
+from flask_restful import Resource, Api, reqparse
 from flask import request
 import flask
 
-@app.route('/api/login', methods=['POST'])
-def login():
-    data        = request.get_json()
-    username    = data['username']
-    password    = data['password']
-    try:
-        user = db.session.query(User).filter_by(email=username).one()
-    except orm.exc.NoResultFound as noUser:
-        return flask.jsonify({"auth_failure": "username/email does not exists"})
-    if user.check_password(password):
-        token = user.generate_token()
-        db.session.add(user)
-        db.session.commit()
-        return flask.jsonify({
-            "status"    :   "login success",
-            "api_token" :   token})
-    else:
-        return flask.jsonify({"auth_failure": "password mismatch"})
 
-@app.route('/api/get_user_details', methods=['POST'])
-def get_details():
-    data        = request.get_json()
-    token       = data['api_token']
-    username    = data['username']
+api = Api(app)
 
-    user = db.session.query(User).filter_by(token=token).one()
+class Login(Resource):
+    def __init__(self):
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('username', type = str, required = True, help = 'no username provided', location = 'json')
+        self.reqparser.add_argument('password', type = str, required = True, help = 'password is required', location = 'json')
+        Resource.__init__(self)
 
-    if not user:
-        return flask.jsonify({"auth_failure": "invalid token"})
-    else:
-        return user.jsonify()
+    def post(self):
+        args = self.reqparser.parse_args()
 
-@app.route('/api/signup', methods=['POST'])
-def signup():
-    data = request.get_json()
-    if not data.get('username'):
-        return flask.jsonify({
-            "api_error": "invalid request"
-            })
+        user = db.session.query(User).filter_by(email=args['username']).all()
+        if not user:
+            return flask.jsonify({"error": "username/email does not exists"})
+        else:
+            if user[0].check_password(args['password']):
+                token = user[0].generate_token()
+                db.session.add(user[0])
+                db.session.commit()
+                return flask.jsonify({
+                        "status"    : "login success",
+                        "api_token" : token
+                    })
+            else:
+                return flask.jsonify({"auth_failure": "password mismatch"})
 
-    elif data.get('check_existing') == True:
+
+class GetDetails(Resource):
+    def __init__(self):
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('api_token', type = str, required = True, help = 'request needs to have your api token', location = 'json')
+        self.reqparser.add_argument('username', type = str, required = True, help = 'request needs to specify username for which details are to be fetched', location = 'json')
+
+    def post(self):
+        args = self.reqparser.parse_args()
+
+        user = db.session.query(User).filter_by(token=args['token']).all()
+
+        if not user:
+            return flask.jsonify({"auth_failure": "invalid token"})
+        else:
+            return user[0].jsonify()
+
+class CheckExistingEmail(Resource):
+    def __init__(self):
+        Resource.__init__(self)
+
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('username', type = str, required = True, location = 'json', help = 'must contain an email for checking')
+
+    def post(self):
+        data = self.reqparser.parse_args()
+
         # check if existing user
         username = data['username']
         try:
             db.session.query(User.email).filter_by(email=username).one()
             return flask.jsonify({
-                "username": username,
-                "is_unique": False
+                    "username": username,
+                    "is_unique": False
                 })
         except orm.exc.NoResultFound as notFound:
             return flask.jsonify({
-                "username": username,
-                "is_unique": True
+                    "username": username,
+                    "is_unique": True
                 })
 
-    else:
+
+class Signup(Resource):
+    def __init__(self):
+        Resource.__init__(self)
+
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('username', type = str, required = True, help = 'must contain the username', location = 'json')
+        self.reqparser.add_argument('firstname', type = str, required = True, help = 'must contain firstname', location = 'json')
+        self.reqparser.add_argument('lastname', type = str, required = True, help = 'must contain lastname', location = 'json')
+        self.reqparser.add_argument('password', type = str, required = True, help = 'password must not be null', location = 'json')
+
+    def post(self):
+        data = self.reqparser.parse_args()
+
         # request for new user signup
-        data = request.get_json()
         firstname = data['firstname']
         lastname = data['lastname']
         email = data['username']
@@ -78,6 +105,11 @@ def signup():
             "status": "signup successful",
             "api_token": user.generate_token()
             })
+
+api.add_resource(Login,'/api/login')
+api.add_resource(GetDetails, '/api/get_user_details')
+api.add_resource(Signup, '/api/signup')
+api.add_resource(CheckExistingEmail, '/api/checkExistingEmail')
 
 @app.route('/')
 @app.route('/login')
