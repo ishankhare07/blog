1b68788cd14475dcb1d8786757f062c5c70167c5
Ishan Khare <ishankhare07@gmail.com>
2016-03-21 18:44:42 +0530

    modified run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from app.main import app, db
+from app.main import app
+from app import db
 
 if __name__ == "__main__":
     with app.app_context():
-        db.init_app(app)
         db.create_all()
         app.run(debug=True)
