c6031f33a508316d4ada453e032598818093ee08
Ishan Khare <ishankhare07@gmail.com>
2016-03-21 19:42:46 +0530

    added a new state and registered the interceptor

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -8,15 +8,27 @@ angular.module('myApp').config(function($stateProvider, $urlRouterProvider, $loc
         url: '/profile',
         templateUrl: 'static/templates/profile.tpl.html',
         controller: 'profileController',
+        controllerAs: 'pc',
     })
 
     .state('home', {
         url: '/',
         template: ''
     })
+
+    .state('editProfile', {
+        url: '/profile/edit',
+        templateUrl: 'static/templates/editProfile.tpl.html',
+        controller: 'editProfileController',
+        controllerAs: 'editCtrl'
+    })
     ;
 });
 
 angular.module('myApp').config(function(localStorageServiceProvider) {
     localStorageServiceProvider.setPrefix('smallBlog');
+})
+
+.config(function($httpProvider) {
+    $httpProvider.interceptors.push('tokenInterceptor');
 });
