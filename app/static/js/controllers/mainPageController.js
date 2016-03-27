da28bf7b1c9b1692077eebd140a4dcde73b9992d
Ishan Khare <ishankhare07@gmail.com>
2016-03-27 19:14:51 +0530

    added the home button in sidebar
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- app/static/js/controllers/mainPageController.js
+++ app/static/js/controllers/mainPageController.js
@@ -1,11 +1,16 @@
-angular.module('myApp').controller('mainPageController', function(loginManager, $mdSidenav) {
+angular.module('myApp').controller('mainPageController', function($location, loginManager, $mdSidenav) {
+    var self = this;
     this.gotoLogin = function(ev) {
         loginManager.presentLogin(ev);
     }
 
     this.leftNav = function() {
-        console.log('toggled');
         $mdSidenav('leftSidenav').toggle();
+    };
+
+    this.navigate = function(url) {
+        $mdSidenav('leftSidenav').close();         // auto-close sidenav after click if open
+        $location.path(url);
     };
 
     this.lm = loginManager;
